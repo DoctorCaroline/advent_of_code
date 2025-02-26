a25938433ac7aa4f0a5231696b85d94453a809ae
Caroline A. Regalado, PhD <caroline.regalado@epic.com>
2025-02-25 22:22:44 -0600

    [Solution] 2015 Day 12 (Part 2)

--- Solutions/2015/Day12.ts
+++ Solutions/2015/Day12.ts
@@ -2,10 +2,28 @@ import * as utils from "../../AdventOfCodeUtils";
 
 /** Day 12 Solution */
 export function solution(rawInput: string[]): utils.Solution {
+
 	let sums = [0, 0];
-	for (const line of rawInput) {
-		const matches = line.matchAll(/-?\d+/g);
-		for (const match of matches) { sums[0] += +match[0]; }
-	}
+	const pojo = rawInput[0];
+
+	// Part 1
+	const matches = pojo.matchAll(/-?\d+/g);
+	for (const match of matches) { sums[0] += +match[0]; }
+
+	// Part 2
+	sums[1] = _evaluate(JSON.parse(pojo));
+	
 	return sums;
 }
+
+function _evaluate(obj: utils.StringKeyedObject<any>): number {
+	let sum = 0;
+	for (const key in obj) {
+		const val = obj[key];
+		if (val === "red" && !(obj instanceof Array)) { return 0; }
+		else if (val instanceof Object) { sum += _evaluate(val); }
+		else if (!isNaN(+val)) { sum += +val; }
+	}
+	return sum;
+}
+
