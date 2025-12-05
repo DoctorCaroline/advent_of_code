03456fcaf80ac672e7c94c085fb1837d590b01bc
Caroline A. Regalado, PhD <caroline.regalado@epic.com>
2025-12-04 23:51:53 -0600

    [Solution] 2025 Day 4 (Optimized)

--- Solutions/2025/Day04.ts
+++ Solutions/2025/Day04.ts
@@ -7,37 +7,45 @@ export function solution(rawInput: string[]): utils.Solution {
 		floor.push(line.split("") as Tile[]);
 	}
 
+	const containsRoll = (orderedPair: number[]) => floor[orderedPair[0]]?.[orderedPair[1]] === "@";
+
 	const out = [0, 0];
 
-	let rollTaken = true;
 	let initialSweep: number[][] = [];
+	let resample: number[][] = [];
+ 
+	for (const row in floor) {
+		for (const col in floor[row]) {
+			if (floor[row][col] !== "@") { continue; }
+			const adjacents = utils._getAdjacents([+row, +col], true);
 
-	while (rollTaken) {
-		rollTaken = false;
+			const neighbors = adjacents.filter(containsRoll);
+			if (neighbors.length >= 4) { continue; }
 
-		for (const row in floor) {
-			for (const col in floor[row]) {
-				if (floor[row][col] !== "@") { continue; }
-				const adjacents = utils._getAdjacents([+row, +col], true);
+			// Subsequent resamples needn't scan entire floor, only neighbors of removed rolls
+			resample.push(...neighbors);
+			out[1]++;
 
-				if (adjacents.filter(adj => floor[adj[0]]?.[adj[1]] === "@").length >= 4) { continue; }
-
-				out[1]++;
-				rollTaken = true;
-
-				if (!out[0]) {
-					// Preserve initial state to accurately count Part 1 (remove after full first pass)
-					initialSweep.push([+row, +col]);
-					continue;
-				}
-				
-				floor[row][col] = ".";
-			}
+			// Preserve initial state to accurately count Part 1
+			initialSweep.push([+row, +col]);
 		}
+	}
 
-		if (out[0]) { continue; }
-		out[0] = initialSweep.length;
-		initialSweep.forEach(coord => floor[coord[0]][coord[1]] = ".");
+	out[0] = initialSweep.length;
+	initialSweep.forEach(coord => floor[coord[0]][coord[1]] = ".");
+
+	// Process resample queue until no further rolls can be removed
+	while (resample.length) {
+		const coord = resample.pop()!;
+		if (!containsRoll(coord)) { continue; }
+		const adjacents = utils._getAdjacents(coord, true);
+
+		const neighbors = adjacents.filter(containsRoll);
+		if (neighbors.length >= 4) { continue; }
+
+		resample.push(...neighbors);
+		out[1]++;
+		floor[coord[0]][coord[1]] = ".";
 	}
 	
 	return out;
